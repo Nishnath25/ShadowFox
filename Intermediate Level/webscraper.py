a38609e1171b9c143f24705e6b10e8e58bdbce50
Nishanth <nishanthnaik15@gmail.com>
2026-05-26 06:52:46 +0530

    Update webscraper.py

--- Intermediate Level/webscraper.py
+++ Intermediate Level/webscraper.py
@@ -1,20 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Website URL
 url = "https://kpmg.com/au/en.html"
 
-# Send request
 response = requests.get(url)
-
-# Parse HTML content
 soup = BeautifulSoup(response.text, "html.parser")
-
-# Find all quote blocks
 headings = soup.find_all("h2")
-
 print("Extracted Headings:")
 
-# Print headings
 for i, headings in enumerate(headings, start=1):
     print(f"{i}. {headings.text}")
