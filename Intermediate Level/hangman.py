ec6c01e76fb3a20df43220af713a2574fddf577f
Nishanth <nishanthnaik15@gmail.com>
2026-05-26 06:51:12 +0530

    Refactor hangman game logic and structure

--- Intermediate Level/hangman.py
+++ Intermediate Level/hangman.py
@@ -1,18 +1,12 @@
 import random
-
-# List of words
 words = ["python", "computer", "hangman", "programming", "developer"]
-
-# Randomly choose a word
 word = random.choice(words)
-
 guessed_letters = []
 attempts = 6
 
-# Hint
 print("Hint: It is related to computers/programming.")
 
-# Display function
+
 def display_word():
     display = ""
     for letter in word:
@@ -22,7 +16,6 @@ def display_word():
             display += "_ "
     return display
 
-# Game loop
 while attempts > 0:
     print("\nWord:", display_word())
     print("Attempts left:", attempts)
@@ -41,7 +34,7 @@ while attempts > 0:
         attempts -= 1
         print("Wrong guess!")
 
-    # Visual Progress
+    # Visual Progress(taken from chatgpt)
     stages = [
         """
            -----
@@ -110,11 +103,9 @@ while attempts > 0:
 
     print(stages[6 - attempts])
 
-    # Check win condition
     if all(letter in guessed_letters for letter in word):
         print("\nCongratulations! You guessed the word:", word)
         break
 
-# Lose condition
 if attempts == 0:
     print("\nGame Over! The word was:", word)
